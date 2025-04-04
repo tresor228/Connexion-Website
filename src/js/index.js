77b21a191ba686b20e8f822f9450c264a3a84b6c
tresor228 <bernardalade92@gmail.com>
2025-04-04 19:19:38 +0000

    mise

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 function verifierReponse(reponse) {
 
     if (reponse === "Oui") {
-        window.location.href = "../page/Connexion.html";
+        window.location.href = "../src/page/Connexion.html";
     } 
     else {
-        window.location.href = "../page/inscription.html";
+        window.location.href = "../src/page/inscription.html";
     }
 }
